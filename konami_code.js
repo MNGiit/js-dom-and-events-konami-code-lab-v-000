b02545a7e1d6e95a0aa507076f382af2dc334179
Mohammed Noori <kmanoorib@gmail.com>
2019-03-08 21:38:00 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ function init() {
   document.body.addEventListener('keydown', onKeyDownHandler);
   
   function onKeyDownHandler(e) {
-    let key = e.parseInt(e.detail || e.which);
+    const key = e.parseInt(e.detail || e.which);
     
     // check if key equals value of code in the correct part of code array
     if (code[index] === key) {
