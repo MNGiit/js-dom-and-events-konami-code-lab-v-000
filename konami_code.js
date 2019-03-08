a6a7e5bea4ba35634c31c022124c352cdce3c580
Mohammed Noori <kmanoorib@gmail.com>
2019-03-08 21:27:42 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -20,7 +20,7 @@ function init() {
     
     // checks if index is equal to code length. if it is, do an alert
     if (index === code.length) {
-      window.alert("Hurray!");
+      console.alert("Hurray!");
       index = 0;
     }
     
