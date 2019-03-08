9ae67c156123183a3358d7bbeefc29de6aa75f02
Mohammed Noori <kmanoorib@gmail.com>
2019-03-08 20:54:28 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -7,16 +7,19 @@ function init() {
   function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
  
-  if (key === alphabet[index]) {
-    index++;
- 
-    if (index === alphabet.length) {
-      alert("Hurray!");
- 
-      index = 0;
+ function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+    console.log('key', key)
+    if (key === code[index]) {
+      index++
+
+       if (index === code.length) {
+        alert("YOU DID IT!")
+        index = 0
+      }
+    } else {
+      index = 0
     }
-  } else {
-    index = 0;
   }
 }
 }
