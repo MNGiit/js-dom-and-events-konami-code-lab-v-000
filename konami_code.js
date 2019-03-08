8f08baf4f15c8406534e9fb2b5003cf9c58cd98e
Mohammed Noori <kmanoorib@gmail.com>
2019-03-08 21:49:33 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -39,7 +39,7 @@ function init() {
 //    
 //    // checks if index is equal to code length. if it is, do an alert
 //    if (index === code.length) {
-//      window.alert("Hurray!");
+//      alert("Hurray!");
 //      index = 0;
 //    }
 //    
