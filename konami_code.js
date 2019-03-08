6c917e56854021cc9c114b98e391c6b500f7b21a
Mohammed Noori <kmanoorib@gmail.com>
2019-03-08 21:17:49 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -6,19 +6,18 @@ function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', onKeyDownHandler);
   
-  
- 
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
- 
+    let index = 0;
+    
     if (key === code[index]) {
       index++;
  
-    if (index === code.length) {
-      alert("Hurray!");
+      if (index === code.length) {
+        alert("Hurray!");
  
-      index = 0;
-    }
+        index = 0;
+      }
     } else {
       index = 0;
     }
